98e0e1fa66b68f25d80172e7a83858f957c40dc9
PROLCY <ckd248@naver.com>
2023-07-04 10:02:31 +0900

    some function is added

--- image_utils.py
+++ image_utils.py
@@ -4,9 +4,11 @@ import os
 import tkinter as tk
 from PIL import Image, ImageTk
 import matplotlib
+import random
 
 from preprocess_data import *
 from files_utils import *
+from hog import *
 
 matplotlib.use('TkAgg')
 
@@ -16,6 +18,7 @@ IMAGE_CNT_BY_ROW = 10
 
 IMAGE_PATH = './data/image/bg-20k-train'
 JSON_PATH = "./data/result"
+DATA_PATH = './data/csv/' + get_config_string() + ';proportion' + '.csv'
 
 # deprecated
 def show_10_images(image_filenames, IMAGE_PATH):
@@ -96,5 +99,41 @@ def create_image_viewer(IMAGE_PATH, labeled_image_files):
 
     window.mainloop()
 
+def show_nearest_images(IMAGE_PATH, target_image_name, nearest_image_name_list):
+
+    window = tk.Tk()
+    window.title("Nearest Image Show")
+
+    canvas = tk.Canvas(window, width=1200, height=700)
+    canvas.pack()
+
+    top_image = Image.open(os.path.join(IMAGE_PATH, target_image_name))
+    top_image = top_image.resize((400, 300))
+    top_image_tk = ImageTk.PhotoImage(top_image)
+
+    canvas.create_image(550, 150, image=top_image_tk)
+
+
+    image_frame = tk.Frame(canvas)
+    canvas.create_window(0, 400, window=image_frame, anchor="nw")
+    for i, image_data in enumerate(nearest_image_name_list):
+        image = Image.open(os.path.join(IMAGE_PATH, image_data[1]))
+        image = image.resize((210, 210))
+        image_tk = ImageTk.PhotoImage(image)
+
+        image_label = tk.Label(image_frame, image=image_tk)
+        image_label.image = image_tk
+        image_label.grid(row=i // 5, column=i % 5, padx=5, pady=5)
+
+        text_label = tk.Label(image_frame, text=str(image_data[0]))
+        text_label.grid(row=i // 5 + 1, column=i % 5, padx=5, pady=5)
+
+    window.mainloop()
+
 if __name__ == '__main__':
-    print(1)
+    name_list = get_name_data_list(DATA_PATH)
+    random.shuffle(name_list)
+    for target_image_name in name_list:
+        nearest_image_name_list = get_nearest_images(target_image_name, get_raw_data_list_hog(DATA_PATH, False), get_name_data_list(DATA_PATH), 5)
+        show_nearest_images(IMAGE_PATH, target_image_name, nearest_image_name_list)
+    
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,6 +1,14 @@
 import numpy as np
 import random
 import math
+import cv2
+import os
+from tqdm import tqdm
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial.distance import euclidean
+
+from hog import *
+
 
 def get_normalized_vector(row):
     vector = eval(row)
@@ -8,6 +16,16 @@ def get_normalized_vector(row):
     if norm == 0:
         return vector
     return [x / norm for x in vector]
+
+def get_middle_point(vector):
+    x1, x2 = vector[0], vector[2]
+    y1, y2 = vector[1], vector[3]
+    return (x1+x2)/2, (y1+y2)/2
+
+def get_gradient(vector):
+    x1, x2 = vector[0], vector[2]
+    y1, y2 = vector[1], vector[3]
+    return (y2-y1)/(x2-x1)
 
 def get_raw_data_list_SAMPNet(DATA_PATH, normalized=False):
     ret = []
@@ -20,6 +38,39 @@ def get_raw_data_list_SAMPNet(DATA_PATH, normalized=False):
             row = row.split('/')[3]
             ret.append((convert_row(row)))
     return np.array(ret)
+
+def append_midpoint_and_gradient(raw_data, line_list):
+    line_list.sort()
+    for line in line_list:
+        midx, midy = get_middle_point(line)
+        raw_data.append(midx)
+        raw_data.append(midy)
+        raw_data.append(get_gradient(line))
+    return raw_data
+
+def get_raw_data_list_DRM(DATA_PATH, normalized=False):
+    ret = []
+    convert_row = eval
+    if(normalized):
+        convert_row = get_normalized_vector
+    with open(DATA_PATH, "r") as file:
+        data_list = file.readlines()
+        for row in data_list:
+            row = row.split('/')
+            out_dict = eval(row[1])['out']
+            pri = out_dict['pri'][0]
+            mul = out_dict['mul'][0]
+            mul.remove(pri)
+            raw_data = []
+            append_midpoint_and_gradient(raw_data, [pri])
+            append_midpoint_and_gradient(raw_data, mul)
+            ret.append(raw_data)
+    max_len = max(len(x) for x in ret)
+    for raw_data in ret:
+        if len(raw_data) < max_len:
+            for cnt in range(max_len - len(raw_data)):
+                raw_data.append(0)
+    return ret
 
 def get_name_data_list(DATA_PATH):
     ret = []
@@ -47,3 +98,100 @@ def get_elements_list_by_label(label_list, name_list, label_cnt):
                 one_label_list.append(name_list[j])
         data_by_label_list.append(one_label_list)
     return data_by_label_list
+
+def get_dict_of_elements_list_by_label(label_list, name_list, label_cnt):
+    data_by_label_list = {}
+    for i in range(label_cnt):
+        one_label_list = []
+        for j in range(len(label_list)):
+            if(label_list[j] == i):
+                one_label_list.append(name_list[j])
+        data_by_label_list[str(i)] = one_label_list
+    return data_by_label_list
+
+def get_vector_from_image(IMAGE_PATH, img_name):
+
+    # Load the image
+    image = cv2.imread(os.path.join(IMAGE_PATH, img_name))
+    print(os.path.join(IMAGE_PATH, img_name))
+    print(image.shape)
+    image = cv2.resize(image, dsize=(100, 100))
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Create a saliency object using the SALIENCY_OTSU algorithm
+    saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
+
+    # Calculate the saliency map
+    success, gray = saliency.computeSaliency(gray)
+
+    # Flatten the image into a 1D vector
+    flattened_image = gray.reshape(-1)
+
+    # Print the shape and the flattened vector
+    print("Flattened image shape:", flattened_image.shape)
+    print("Flattened image vector:", flattened_image)
+
+    return flattened_image.tolist()
+
+def get_raw_data_list_temp_vector(DATA_PATH, normalized=False):
+    ret = []
+    convert_row = eval
+    if(normalized):
+        convert_row = get_normalized_vector
+    with open(DATA_PATH, "r") as file:
+        data_list = file.readlines()
+        for row in data_list:
+            row = row.split('/')[1]
+            ret.append((convert_row(row)))
+    return np.array(ret)
+
+def get_distance(vector1, vector2):
+    vector1 = np.array(vector1)
+    vector2 = np.array(vector2)
+    
+    vector1 = vector1.reshape(1, -1)
+    vector2 = vector2.reshape(1, -1)
+    similarity = cosine_similarity(vector1, vector2)
+    
+    # similarity = euclidean(vector1, vector2)
+    return similarity
+
+def get_raw_data_list_hog(DATA_PATH, normalized=False):
+    ret = []
+    convert_row = eval
+    if(normalized):
+        convert_row = get_normalized_vector
+    with open(DATA_PATH, "r") as file:
+        data_list = file.readlines()
+        for row in data_list:
+            row = row.split('/')[1]
+            ret.append((convert_row(row)))
+    return np.array(ret)    
+
+def get_nearest_images(target_image_name, raw_data_list, name_list, nearest_image_cnt):
+    target_image_data = raw_data_list[name_list.index(target_image_name)]
+    data_and_name_list = []
+    for i in range(len(name_list)):
+        if name_list[i] == target_image_name:
+            continue
+        distance = euclidean(target_image_data, raw_data_list[i])
+        data_and_name_list.append([distance, name_list[i], raw_data_list[i]])
+    data_and_name_list.sort()
+    return data_and_name_list[:nearest_image_cnt]
+
+if __name__ == '__main__':
+    image_list = os.listdir('./data/image/bg-20k-train')
+    if '.DS_Store' in image_list:
+        image_list.remove('.DS_Store')
+    hog_list = []
+    print(image_list)
+    filename = get_config_string() + ';proportion' + '.csv'
+    if os.path.exists('./data/csv/' + filename):
+        print("There exists file!")
+    else:
+        for img in tqdm(image_list):
+            with open('./data/csv/' + filename, 'a') as f:
+                f.writelines(img + '/' + str(get_hog("./data/image/bg-20k-train", img)) + '\n')
+
+    
